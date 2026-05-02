949221fda5e54d5d4e440e070534e2ce8a6e0579
ramiledov <ramiledov@gmail.com>
2026-05-02 19:51:22 +0400

    Remove duplicate nested website folder and keep site at repository root

--- mypersonalwebsite_info-1101/js/main.js
+++
@@ -1,9 +0,0 @@
-// Main initialization module
-import { initRaceGame } from './modules/game.js';
-import { initProjectGallery } from './modules/gallery.js';
-
-window.addEventListener('load', function() {
-    console.log('Portfolio loaded successfully!');
-    initRaceGame();
-    initProjectGallery();
-});
